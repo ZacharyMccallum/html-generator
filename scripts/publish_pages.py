76d5ac9cc5121aacb65f941f90da98f7d570c86d
ZacharyMccallum <z19mccallum@gmail.com>
2022-11-28 17:30:27 -0500

    Update publish_pages.py
    adjusted var names for clarity. tested webbrowser open functionality - Fails due to mainmenu.html not existing -Yet!

--- scripts/publish_pages.py
+++ scripts/publish_pages.py
@@ -1,5 +1,5 @@
 import webbrowser, os
-stu_dict = {
+dict1 = {
     'Zachary McCallum' : {'FName' : 'Zachary', 'LName' : 'McCallum'}
     ,'Alec Brown' : {'FName' : 'Alec', 'LName' : 'Brown'}
     , 'Navkiran Singh' : {'FName' : 'Navkiran', 'LName' : 'Singh'}
@@ -13,11 +13,13 @@ html_paths = {
 }
 
 def publishPages(stu_dict):
-    for stu_name, stu_info in stu_dict.itemms():
+    for stu_name, stu_info in stu_dict.items():
         print("\nStudent Name: ", stu_name)
-        
+
         for key in stu_info:
             filename = key +".html"
 
     #Open webrowser to the main menu html page
     webbrowser.open('file://' + os.path.realpath('mainmenu.html'))
+
+publishPages(dict1)
