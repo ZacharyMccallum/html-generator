d211352176eaec614f96e3891689afe422686ce2
brownalec <116525986+brownalec@users.noreply.github.com>
2022-11-29 17:12:12 -0500

    Update GeneratingHTML.py

--- scripts/GeneratingHTML.py
+++ scripts/GeneratingHTML.py
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <link rel="stylesheet" href="projectstyle.css">
+  <head>
+    <meta charset="utf-8" />
+    <title>Main-Menu</title>
+  </head>
 
+
+
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="link 1.html">Ethelin Awton</a>
+    <a href="#">Ricard Laurant</a>
+    <a href="#">Carlynn Rawes</a>
+    
+    </div>
+</div>
+
+</html>
