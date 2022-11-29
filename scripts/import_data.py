5c86a5d82c7c0c3d474dec77076d0f90d94c3bce
ZacharyMccallum <z19mccallum@gmail.com>
2022-11-29 18:07:35 -0500

    Update import_data.py
    copy Navkiran's code from another source

--- scripts/import_data.py
+++ scripts/import_data.py
@@ -2,8 +2,14 @@
 import openpyxl
 
 def readData():
-    wb = openpyxl.load_workbook(r'./src/user_details.xlsx')
-    print(type(wb))
-    
+    workbook = openpyxl.load_workbook(r'./src/user_details.xlsx')
+    data = workbook.active
 
-readData()
+    for i in range(1, data.max_row + 1):
+        print("\n")
+        print("Row ", i, " data :")
+
+        for j in range(1, data.max_column + 1):
+            cell_obj = data.cell(row=i, column=j)
+            print(cell_obj.value, end=" ")
+#readData()
