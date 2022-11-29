bfb8f001071bc8c4aabf0105660b99ed7854e026
ZacharyMccallum <z19mccallum@gmail.com>
2022-11-29 17:21:53 -0500

    major feature changes
    improved menu

--- scripts/main.py
+++ scripts/main.py
@@ -3,42 +3,56 @@ from publish_pages import publishPages
 from import_data import readData
 
 def main():
+
+    # [START REMOVE]
+                # hardcoded dictionary, needed for testing of option [3]
+                # Expected format for output of case 2
+    student_dictionary = {
+            'Zachary McCallum': 
+                {'filename': './mccallum'}
+            ,'Alec Brown':
+                {'filename': './brown.html'}
+            , 'Navkiran Singh':
+                {'filename': './brown.html'}
+            }
+# [END REMOVE]
+# [START REMOVE]
+                # hardcoded dictionary, needed for testing of option [2]
+                # Expected format for output of case 2
+    xlsx_contents = {
+        'Zachary McCallum' : 
+            { 'FName' : 'Zachary', 'FName_format' : 'Bold'
+            , 'LName' : 'McCallum', 'LName_format' : 'Normal'}
+        ,'Alec Brown':
+            {'FName' : 'Alec', 'FName_format' : 'Bold'
+            , 'LName' : 'Brown', 'LName_format' : 'Normal'}
+        , 'Navkiran Singh':
+            {'FName' : 'Navkiran', 'FName_format' : 'Bold'
+            , 'LName' : 'Singh', 'LName_format' : 'Normal'}
+        }
+# [TODO]
+                # Please include+test this in your code, i think the href may need to be messed with a bit
+#                <footer><p>Author: Team 3 - CSI 3680</p><p><a href='mainmenu.html'>Return Home</a></p></footer>
+# [END REMOVE]
     # Set default if enter is chosen
-    user_input = 1
-    while user_input != 99:
+    user_input = "0"
+    while user_input != "99":
         print("XSLX->HTML Generator Menu".center(30, "-"))
         print("Please select an option:\n".center(30))
         print("[1]: Import data from file\n")
         print("[2]: Generate HTML Content\n")
         print("[3]: Publish Pages\n")
         print("[99]: Exit")
-        user_input = int(input("\n->"))
+        user_input = str(input("\n->"))
+        print(user_input)
+        
         match user_input:
-            case 1:
+            case "1":
                 # Navkiran Singh's Function
                 print("Option 1 selected")
                 xlsx_contents = readData()
-            case 2:
+            case "2":
                 # Alec Brown Function
-# [START REMOVE]
-                # hardcoded dictionary, needed for testing of option [2]
-                # Expected format for output of case 2
-                xlsx_contents = {
-                    'Zachary McCallum' : 
-                        { 'FName' : 'Zachary', 'FName_format' : 'Bold'
-                        , 'LName' : 'McCallum', 'LName_format' : 'Normal'}
-                    ,'Alec Brown':
-                        {'FName' : 'Alec', 'FName_format' : 'Bold'
-                        , 'LName' : 'Brown', 'LName_format' : 'Normal'}
-                    , 'Navkiran Singh':
-                        {'FName' : 'Navkiran', 'FName_format' : 'Bold'
-                        , 'LName' : 'Singh', 'LName_format' : 'Normal'}
-                        }
-# [TODO]
-                # Please include+test this in your code, i think the href may need to be messed with a bit
-#                <footer><p>Author: Team 3 - CSI 3680</p><p><a href='mainmenu.html'>Return Home</a></p></footer>
-# [END REMOVE]
-
                 # Check to make sure the contents have been read-in from the xlsx .file first
                 if xlsx_contents is not None:
                     print("Option 2 selected")
@@ -47,21 +61,8 @@ def main():
                 else:
                     print('Please Select option 1 first')
 
-            case 3:
+            case "3":
                 # Zachary McCallum's Function
-
-# [START REMOVE]
-                # hardcoded dictionary, needed for testing of option [3]
-                # Expected format for output of case 2
-                student_dictionary = {
-                    'Zachary McCallum': 
-                        {'filename': './mccallum'}
-                    ,'Alec Brown':
-                        {'filename': './brown.html'}
-                    , 'Navkiran Singh':
-                        {'filename': './brown.html'}
-                        }
-# [END REMOVE]
                 if student_dictionary is not None:
                     print("Option 2 selected")
                     publishPages(student_dictionary)
@@ -69,8 +70,10 @@ def main():
                     print('Please Select option 1 first')
 
             # Define Default Case (No option selected)
+            case 0:
+                print('Loading')
             case _:
-                print("No option selected")
+                print("Please enter your selection")
             
 # Call the main function on script execution
 if __name__ == "__main__":
--- scripts/publish_pages.py
+++ scripts/publish_pages.py
@@ -19,14 +19,15 @@ def publishPages(stu_dict):
     # Access all elements of the outer dictionary, store keys into stu_name, store values into stu_info
     for stu_name, stu_info in stu_dict.items():
         print("Generating page for:\n", stu_name)
-        for key, val in stu_info.items():
-            if key == "filename":
-                filename = str(val)
-                # Save the students name to list to use in table generation
-                students_list.append(stu_name)
-                # Save the file name to a list to use in table generation
-                files_list.append(filename)
+        # Access all elements of the 
+        for val in stu_info.values():
+            filename = str(val)
+            # Save the students name to list to use in table generation
+            students_list.append(stu_name)
+            # Save the file name to a list to use in table generation
+            files_list.append(filename)
     # Check to make sure the files are all collected
+    print(students_list)
     print(files_list)
 
     #HTML Table creation
@@ -35,13 +36,12 @@ def publishPages(stu_dict):
     for student in students_list:
         for link in files_list:
             # Check to make sure the student and link are alike before adding a new tabel row
-            if student in link:
+            if student.endswith(link):
                 strRW = "<tr><td>"+str(student)+"</td><td>"+f"<a href={str('./'+link)}>"+str(link)+"</a>"+"</td></tr>"
                 # Add new row to table
                 strTable = strTable+strRW
             else:
                 continue
-            
  
     strTable = strTable+"</table><footer><p>Author: Team 3 - CSI 3680</p><p><a href='mainmenu.html'>Return Home</a></p></footer></html>"
  
@@ -49,6 +49,5 @@ def publishPages(stu_dict):
     print(strTable)
     f.write(strTable)
     f.close()
-
-# Open webrowser to the main menu html page
-webbrowser.open('file://' + os.path.realpath('./web/mainmenu.html'))
+    # Open webrowser to the main menu html page
+    webbrowser.open('file://' + os.path.realpath('./web/mainmenu.html'))
