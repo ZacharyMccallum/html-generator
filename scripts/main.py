7b2af5d30c3bee6d1f7e069f6235695adb5ca52a
ZacharyMccallum <z19mccallum@gmail.com>
2022-11-28 21:00:56 -0500

    Update main.py
    added menu system

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,40 @@
+import format_html
+from publish_pages import publishPages
+from import_data import readData
+
 def main():
-    print("HTML Generator Menu".center(4, "-"))
-    print("")
+    # Set default if enter is chosen
+    user_input = 1
+    while user_input != 99:
+        print("XSLX->HTML Generator Menu".center(30, "-"))
+        print("Please select an option:\n".center(30))
+        print("[1]: Import data from file\n")
+        print("[2]: Generate HTML Content\n")
+        print("[3]: Publish Pages\n")
+        print("[99]: Exit")
+        user_input = int(input("\n->"))
+        match user_input:
+            case 1:
+                # Navkiran Singh's Function
+                print("Option 1 selected")
+                readData()
+            case 2:
+                # Alec Brown Function
+                print("Option 2 selected")
+            case 3:
+                # Zachary McCallum's Function
+                print("Option 3 selected")
+                # hardcoded dictionary, need for testing of option [3]
+                student_dictionary = {
+    'Zachary McCallum' : {'FName' : 'Zachary', 'LName' : 'McCallum'}
+    ,'Alec Brown' : {'FName' : 'Alec', 'LName' : 'Brown'}
+    , 'Navkiran Singh' : {'FName' : 'Navkiran', 'LName' : 'Singh'}
+}
+                publishPages(student_dictionary)
+            # Define Default Case (No option selected)
+            case _:
+                print("No option selected")
+            
+
+if __name__ == "__main__":
+    main()
